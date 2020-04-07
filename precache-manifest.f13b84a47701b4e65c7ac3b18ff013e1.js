3606059bccfcf158ef8d818b53305a39d48be11b
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-04-06 18:56:21 -0700

    Updates

---
+++ precache-manifest.f13b84a47701b4e65c7ac3b18ff013e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6466f140f6b536071f6cf9d79058f47",
+    "url": "/chirper/index.html"
+  },
+  {
+    "revision": "93c2e193d301e9e9812f",
+    "url": "/chirper/static/css/main.038089b0.chunk.css"
+  },
+  {
+    "revision": "747d59a711787da6ef00",
+    "url": "/chirper/static/js/2.fc41dffb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chirper/static/js/2.fc41dffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c2e193d301e9e9812f",
+    "url": "/chirper/static/js/main.e0621872.chunk.js"
+  },
+  {
+    "revision": "0ebe7adf639610d52afc",
+    "url": "/chirper/static/js/runtime-main.bdc866e8.js"
+  }
+]);
