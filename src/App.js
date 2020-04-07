f012dfbdb679da99543aaf97c76c38dc5b5b64d3
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-04-07 15:36:44 -0700

    feat: The application connected to the store to receive contents
    - app.js changed to container and used connect
    - dispatch called to call handleIntialData action creator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './styles/App.scss';
 import {Col, Container, Row, Spinner} from "react-bootstrap";
+import {connect} from "react-redux";
+import {handleInitialData} from "./actions/shared.actions";
 
-function App() {
-  return (
-      <Container fluid >
-          <Row>
-              <Col className="center">
-                  <h1 className="header-text">Chirper Application!</h1>
-                  <Spinner animation="border" role="status">
-                      <span className="sr-only">Loading...</span>
-                  </Spinner>
-              </Col>
-          </Row>
-      </Container>
-  );
+class App extends Component {
+
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+    }
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col className="center">
+                        <h1 className="header-text">Chirper Application!</h1>
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
-export default App;
+export default connect()(App);
